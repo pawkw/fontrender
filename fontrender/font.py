990023d2b234194238b0143fdfc4baed54ca8095
pawkw <pawkw@users.noreply.github.com>
2024-10-01 00:01:41 -0400

    Data outlines

--- fontrender/font.py
+++ fontrender/font.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import struct
 
 @dataclass
 class directory_entry:
@@ -8,14 +9,29 @@ class directory_entry:
     length: int
 
     def __init__(self, bytes: bytearray):
-        tag = ''
-        for index in range(4):
-            tag += chr(bytes[index])
+        tag, checksum, offset, length = struct.unpack('>4sIII', bytes)
 
-        self.tag = tag
-        self.checksum = int.from_bytes(bytes[4:8])
-        self.offset = int.from_bytes(bytes[8:12])
-        self.length = int.from_bytes(bytes[12:16])
+        self.tag = tag.decode('ascii')
+        self.checksum = checksum
+        self.offset = offset
+        self.length = length
+
+@dataclass
+class Glyph:
+    numberOfContours: int
+    xMin: int
+    yMin: int
+    xMax: int
+    yMax: int
+
+    def __init__(self, bytes: bytearray):
+        numberOfContours, xMin, yMin, xMax, yMax = struct.unpack('>IIIII', bytes[0:12])
+        self.numberOfContours = numberOfContours
+        self.xMin = xMin
+        self.yMin = yMin
+        self.xMax = xMax
+        self.yMax = yMax
+
 
 class PWFont:
     def __init__(self, bytes: bytearray) -> None:
@@ -30,13 +46,14 @@ class PWFont:
         for entry in range(self.offset_table['numTables']):
             offset = index+(entry*16)
             self.directory_entries.append(directory_entry(self.bytes[offset:offset+16]))
-            print(self.directory_entries[-1].tag)
+            print(f'entry: {self.directory_entries[-1].tag} location: {self.directory_entries[-1].offset}')
 
         
     def get_offset_table(self):
         self.offset_table = {}
-        self.offset_table['scaler_type'] = int.from_bytes(self.bytes[0:4], byteorder='big')
-        self.offset_table['numTables'] = int.from_bytes(self.bytes[4:6], byteorder='big')
-        self.offset_table['searchRange'] = int.from_bytes(self.bytes[6:8], byteorder='big')
-        self.offset_table['entrySelector'] = int.from_bytes(self.bytes[8:10], byteorder='big')
-        self.offset_table['rangeShift'] = int.from_bytes(self.bytes[10:12], byteorder='big')
+        
+        self.offset_table['scaler_type'] = struct.unpack('>I', self.bytes[0:4])[0]
+        self.offset_table['numTables'] = struct.unpack('>H', self.bytes[4:6])[0]
+        self.offset_table['searchRange'] = struct.unpack('>H', self.bytes[6:8])[0]
+        self.offset_table['entrySelector'] = struct.unpack('>H', self.bytes[8:10])[0]
+        self.offset_table['rangeShift'] = struct.unpack('>H', self.bytes[10:12])[0]
